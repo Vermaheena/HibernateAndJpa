c115399e8ee03823165b57e8ae5cba95923568e4
heena verma <verma.heena931@gmail.com>
2020-07-19 11:29:09 +0530

    inital commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+create table person
+(
+  id integer not null,
+  birth_date timestamp,
+  location varchar(255),
+  name varchar(255),
+  primary key (id)
+);
+INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE ) VALUES(10001,  'Ranga', 'Hyderabad',sysdate());
+INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE ) VALUES(10002,  'James', 'New York',sysdate());
+INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE ) VALUES(10003,  'Pieter', 'Amsterdam',sysdate());
+INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE ) VALUES(10004,  'Heena', 'Lucknow',sysdate());
+INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE ) VALUES(10005,  'Debu', 'New Delhi',sysdate());
+INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE ) VALUES(10006,  'Aashi', 'Kanpur',sysdate());
